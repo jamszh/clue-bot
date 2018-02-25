ecee3a59703741e242dba6c9ec824544cda09b7e
Yumleg <jamesz96.au@gmail.com>
2018-02-25 22:15:05 +1100

    Modified tester to confirm super rare drops

--- test_app.js
+++ test_app.js
@@ -8,8 +8,15 @@ var formatter = require('./formatter');
 var level = 'hard';
 var roller = new Roller(level);
 
-Sanitiser(roller.roll()).then(function(data){
-	var out = formatter(data);
+
+// Sanitiser(roller.roll()).then(function(data){
+// 	var out = formatter(data);
+// 	console.log(out);
+// })
+
+
+for(var i = 0; i < 10000; i++){
+	var out = roller.roll();
 	console.log(out);
-})
+}
 
